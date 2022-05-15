5f2ae8a169979c9c31b630032f5584a82085b36c
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-05-15 12:48:12 +0200

    Create task2.c

---
+++ task2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+// the behavior of %i and %d
+// with printf statement
+
+ 
+int main()
+{
+    int num = 9;
+     
+    // print value using %d
+    printf("Value of num using %%d is = %d\n", num);
+     
+    // print value using %i
+    printf("Value of num using %%i is = %i\n", num);
+ 
+    return 0;
+}
