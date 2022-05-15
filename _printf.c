28ecf4d5a4f7056ff5b60c12b28450d96d4c501c
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-05-15 12:39:31 +0200

    task 00

--- _printf.c
+++ _printf.c
@@ -7,18 +7,3 @@
  * @c: char to check
  * Return: pointer to function
  */
-int main () {
-   FILE *fp;
-   int c;
-
-   fp = fopen("file.txt","r");
-   while(1) {
-      c = fgetc(fp);
-      if( feof(fp) ) {
-         break;
-      }
-      printf("%c", c);
-   }
-   fclose(fp);
-   return(0);
-}
