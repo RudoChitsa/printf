b741e3036b4c3f36913e79e9a9fe7ba8a56fff92
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-05-17 16:18:43 +0200

    00

--- _printf.c
+++ _printf.c
@@ -9,72 +9,38 @@
  * void *addr;
  * Return: nbytes printed.
  */
-int _printf(const char *format, ...)
+int _printf(const char *format, ...);
 {
-	va_list list;
-	unsigned int i = 0, characters_number = 0;
+    int len;
+    int len2;
+    unsigned int ui;
+    void *addr;
 
-	if (!format)
-		return (-1);
-
-	va_start(list, format);
-	for (i = 0; format[i] != '\0'; i++)
-	{
-		if (format[i] == '%')
-		{
-			if (format[i + 1] == '\0')
-				return (-1);
-
-			else if (format[i + 1] == '%')
-			{
-				_putchar('%');
-				characters_number++;
-				i++;
-			}
-			else if (cmp_func(format[i + 1]) != NULL)
-			{
-				characters_number += (cmp_func(format[i + 1]))(list);
-				i++;
-			}
-			else
-			{
-				_putchar(format[i]);
-				characters_number++;
-			}
-		}
-		else
-		{
-			_putchar(format[i]);
-			characters_number++;
-		}
-	}
-	va_end(list);
-	return (characters_number);
-}
-
-/**
-
- * Return: 0.
- */
-int (*cmp_func(const char a))(va_list)
-{
-	print_f printf[] = {
-		{'c', printc},
-		{'s', print_string},
-		{'d', print_n},
-		{'i', print_n},
-		{'\0', NULL}
-	};
-
-	int k;
-
-	for (k = 0; printf[k].p != '\0'; k++)
-	{
-		if (printf[k].p == a)
-		{
-			return (printf[k].func);
-		}
-	}
-
-	return (0);
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    ui = (unsigned int)INT_MAX + 1024;
+    addr = (void *)0x7ffe637541f0;
+    _printf("Length:[%d, %i]\n", len, len);
+    printf("Length:[%d, %i]\n", len2, len2);
+    _printf("Negative:[%d]\n", -762534);
+    printf("Negative:[%d]\n", -762534);
+    _printf("Unsigned:[%u]\n", ui);
+    printf("Unsigned:[%u]\n", ui);
+    _printf("Unsigned octal:[%o]\n", ui);
+    printf("Unsigned octal:[%o]\n", ui);
+    _printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    printf("Unsigned hexadecimal:[%x, %X]\n", ui, ui);
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("Address:[%p]\n", addr);
+    printf("Address:[%p]\n", addr);
+    len = _printf("Percent:[%%]\n");
+    len2 = printf("Percent:[%%]\n");
+    _printf("Len:[%d]\n", len);
+    printf("Len:[%d]\n", len2);
+    _printf("Unknown:[%r]\n");
+    printf("Unknown:[%r]\n");
+    return (0);
 }
