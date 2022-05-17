d2b8a3218f79219caaeb9c01c9c0a74548bf5416
RudoChitsa <47056287+RudoChitsa@users.noreply.github.com>
2022-05-17 16:26:22 +0200

    Update _printf.c

--- _printf.c
+++ _printf.c
@@ -1,19 +1,78 @@
-#include <limits.h>
-#include <stdio.h>
 #include "main.h"
 /**
+ * _printf - printf function.
  * @format: variable
- * int len;
- * int len2;
- * unsigned int ui;
- * void *addr;
+ *
  * Return: nbytes printed.
  */
-int _printf(const char *format, ...);
+int _printf(const char *format, ...)
 {
-     printf("1234567890123%n4567890123456789\n\n", &count);          
-    printf("Value of count should be 13; count = %d\n\n", count);   
-    printf("%10c%5c\n", ch, ch);                                    
-    printf("%25s\n%25.4s\n\n", string, string);                     
-    printf("%f    %.2f    %e    %E\n\n", fp, fp, fp, fp);           
-    printf("%i    %i     %i\n\n", hex, oct, dec); 
+	va_list list;
+	unsigned int i = 0, characters_number = 0;
+
+	if (!format)
+		return (-1);
+
+	va_start(list, format);
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i + 1] == '\0')
+				return (-1);
+
+			else if (format[i + 1] == '%')
+			{
+				_putchar('%');
+				characters_number++;
+				i++;
+			}
+			else if (cmp_func(format[i + 1]) != NULL)
+			{
+				characters_number += (cmp_func(format[i + 1]))(list);
+				i++;
+			}
+			else
+			{
+				_putchar(format[i]);
+				characters_number++;
+			}
+		}
+		else
+		{
+			_putchar(format[i]);
+			characters_number++;
+		}
+	}
+	va_end(list);
+	return (characters_number);
+}
+
+/**
+ * cmp_func - Entry point
+ * @a: character.
+ *
+ * Return: 0.
+ */
+int (*cmp_func(const char a))(va_list)
+{
+	print_f printf[] = {
+		{'c', printc},
+		{'s', print_string},
+		{'d', print_n},
+		{'i', print_n},
+		{'\0', NULL}
+	};
+
+	int k;
+
+	for (k = 0; printf[k].p != '\0'; k++)
+	{
+		if (printf[k].p == a)
+		{
+			return (printf[k].func);
+		}
+	}
+
+	return (0);
+}
